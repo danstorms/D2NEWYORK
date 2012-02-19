9539b9bea7fbe598015510eab05ccdf0f2c1e6ba
Dan Storms <danstorms@gmail.com>
2012-02-19 17:21:35 -0500

    adding all intro files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+D2newyork::Application.config.secret_token = '8837a6b9acd0e3bfd2b24bf792d40ae37d408d325404e3d2ea57067e33ecf703b6e4b2c8d5650c8a40bbcdc1467c0f412cb9b4bc106e16728794731773ee8a54'
