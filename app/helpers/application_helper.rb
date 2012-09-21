c4553572a9bf2b8fec0f5b9a87e6d1d9228b95d7
Dan Storms <danstorms@gmail.com>
2012-09-20 22:43:13 -0400

    added sketchy code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,72 @@ module ApplicationHelper
     
   end
   
+  # Dan's testing
+  def myresult
+    
+    problem_3
+    
+  end
+  
+  def problem_3
+    
+    i = 3
+#    num = 30
+#    num = 13195
+#    num = 51475143
+    num = 600851475143
+    is_prime = true
+    primes = [2]
+    
+    while i < num/2
+      
+#      puts "STARTING " << i.to_s()
+      
+      unless num%2 == 0
+      
+        if (num%i == 0)
+        
+          primes.each do |prime|
+            if ( is_prime == true && i%prime == 0) # i isn't prime
+              is_prime = false
+            
+              puts "it's divisible by " + i.to_s() + " but that's not prime"
+            
+              break 
+            end
+          end
+        
+          primes.push i unless is_prime == false
+        
+#          puts "primes are: " << primes.to_s()
+        end
+      
+      end
+            
+      i+=1
+      is_prime = true
+      
+    end
+    
+    primes.last
+    
+  end
+  
+  def problem_1
+    
+    i = 3
+    total = 0
+
+    while i < 1000
+
+      if (i%3 == 0 || i%5 == 0)
+        total += i
+      end
+      i+=1
+    end
+    
+    total
+        
+  end
+  
 end
