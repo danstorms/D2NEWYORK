8f35bec286ef7a7ec4b59c57b1e0efe0fef20246
Dan Storms <danstorms@gmail.com>
2013-07-06 16:54:05 -0400

    removed asset compile to see if that gets the app working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ D2newyork::Application.configure do
 
   #  --Don't fallback to assets pipeline if a precompiled asset is missed
   ## DO fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
